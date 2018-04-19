f65265e4f8c988189234d9a939ff64c3228ee59a
Ashlon Hill <38532149+BagpipeAsh@users.noreply.github.com>
2018-04-19 08:16:30 -0600

    uploaded files

---
+++ upvote/app/seed.js
@@ -0,0 +1,51 @@
+window.Seed = (function () {
+  const submissions = [
+    {
+      id: 1,
+      title: 'Basil',
+      description: 'Feed yo\' kids',
+      url: '#',
+      votes: 16,
+      avatar: '../public/images/avatars/ash.jpg',
+      submissionImage: '../public/images/submissions/basil.png',
+    },
+    {
+      id: 2,
+      title: 'Serenity',
+      description: 'Put yo\' kids to sleep',
+      url: '#',
+      votes: 11,
+      avatar: '../public/images/avatars/kristy.png',
+      submissionImage: '../public/images/submissions/image-rose.png',
+    },
+    {
+      id: 3,
+      title: 'Past Tense',
+      description: 'Yo\' kid got a headache?',
+      url: '#',
+      votes: 17,
+      avatar: '../public/images/avatars/veronika.jpg',
+      submissionImage: '../public/images/submissions/image-steel.png',
+    },
+    {
+      id: 4,
+      title: 'Purify',
+      description: 'Yo\' kid got some Stinky shoes?',
+      url: '#',
+      votes: 9,
+      avatar: '../public/images/avatars/molly.png',
+      submissionImage: '../public/images/submissions/image-aqua.png',
+    }, 
+    {
+      id: 5,
+      title: 'OnGaurd',
+      description: 'Yo\' kid got some nasty cough.',
+      url: '#',
+      votes: 9,
+      avatar: '../public/images/avatars/molly.png',
+      submissionImage: '../public/images/submissions/image-aqua.png',
+    }
+  ];
+
+  return { submissions: submissions };
+}());
